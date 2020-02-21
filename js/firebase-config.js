e9e16f17a93a63faf60dff0040225c0962294f79
Ehmad <justEhmadSaeed@gmail.com>
2020-02-21 15:15:52 +0500

    Initial Commit

---
+++ js/firebase-config.js
@@ -0,0 +1,9 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBbIIs3_AfmhQoAb61NHqPoh9C1XKkfLrM",
+    authDomain: "reddocks-80f49.firebaseapp.com",
+    databaseURL: "https://reddocks-80f49.firebaseio.com",
+    projectId: "reddocks-80f49",
+    storageBucket: "reddocks-80f49.appspot.com",
+    messagingSenderId: "627913487590",
+    appId: "1:627913487590:web:fef2513202bf12e12eb727"
+  };
